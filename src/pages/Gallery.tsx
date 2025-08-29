8fc95b3eb2b40e4f17c5294e5b1dd23ea9779cb0
jitendra8911 <jitendra8911@gmail.com>
2025-08-29 00:03:26 -0500

    update gallery layout

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Grid, Card, CardMedia } from "@mui/material";
+import {Box, Typography, Grid, Card, CardMedia, CardContent} from "@mui/material";
 import "../styles/pages/gallery.scss";
 
 const images: string[] = [
@@ -12,24 +12,37 @@ const Gallery: React.FC = () => {
     return (
         <Box className="page-container">
             <Box className="page-content">
-                <Typography variant="h4" gutterBottom>
-                    Gallery 🏡
-                </Typography>
-
-                <Grid container spacing={2}>
-                    {images.map((src, i) => (
-                        <Grid  key={`gallery-img-${i}`} component="div">
-                            <Card sx={{ height: "100%" }}>
-                                <CardMedia
-                                    component="img"
-                                    image={src}
-                                    alt={`House progress ${i + 1}`}
-                                    sx={{ height: 240, objectFit: "cover" }}
-                                />
-                            </Card>
+                <Card  sx={{
+                    maxWidth: 600,
+                    margin: "0 auto",
+                    borderRadius: 3,
+                    boxShadow: "none",
+                    border: "none",
+                    backgroundColor: "rgba(255,255,255,0.85)", // semi-transparent for background blending
+                }}>
+                    <CardContent>
+                        <Typography variant="h4" gutterBottom>
+                            Gallery 🏡
+                        </Typography>
+                        <Grid container spacing={2}>
+                            {images.map((src, i) => (
+                                <Grid  key={`gallery-img-${i}`} component="div">
+                                    <Card sx={{ height: "100%" }}>
+                                        <CardMedia
+                                            component="img"
+                                            image={src}
+                                            alt={`House progress ${i + 1}`}
+                                            sx={{ height: 240, objectFit: "cover" }}
+                                        />
+                                    </Card>
+                                </Grid>
+                            ))}
                         </Grid>
-                    ))}
-                </Grid>
+                    </CardContent>
+                </Card>
+
+
+
             </Box>
         </Box>
     );
