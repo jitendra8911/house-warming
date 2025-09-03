f09735121e94bc0837b773ce8b666bd3b4444f9e
jitendra8911 <jitendra8911@gmail.com>
2025-09-03 09:56:31 -0500

    request attendees to bring jackets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -96,6 +96,12 @@ const Home: React.FC = () => {
                                     2936 Red Rose Road, Aurora, IL, 60503
                                 </Typography>
                             </Typography>
+
+                            <Typography variant="subtitle1" sx={{ mt: 1.5 }} >
+                                <Typography component="span" fontWeight={300}>
+                                    ❄️ Please bring jackets — the weather tends to get chilly around this time!
+                                </Typography>
+                            </Typography>
                         </Box>
 
                         <Dialog open={openInvitation}
